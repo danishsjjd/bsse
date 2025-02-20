07429016ab4a12157f1d577eb0509663e9d525ce
Danish Sajjad <92915951+Danishsjjd@users.noreply.github.com>
2025-02-20 09:28:00 +0500

    feat: semester 1 & 2 content

---
+++ semester 2/CS201 Introduction to Programming/lectures/10/main.cpp
@@ -0,0 +1,12 @@
+#include <cmath>
+
+#include "global"
+
+void square(double *named);
+
+int main() {
+  double x = 25;
+  double squareRoot = sqrt(x);
+
+  return 0;
+}
---
+++ semester 2/CS201 Introduction to Programming/lectures/12/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+void reverse(char arr[], int size) {
+  int start = 0;
+  int end = size - 2;  // -2 because size includes null terminator
+  while (start < end) {
+    // Swap characters
+    char temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
+}
+
+int main() {
+  // int number1 = 1;
+  // int number2 = 2;
+
+  // // Explicitly indicate we're ignoring the result
+  // (void)(number1 << number2);
+
+  // std::cout << "number1: " << number1 << std::endl;
+  // std::cout << "number2: " << number2 << std::endl;
+
+  // char a_string[20] = "This is a string very awesome";
+
+  char hello[] = "Hello world!";
+  std::cout << "Before reverse: " << hello << std::endl;
+  reverse(hello, sizeof(hello));
+  std::cout << "After reverse: " << hello << std::endl;
+
+  return 0;
+}
